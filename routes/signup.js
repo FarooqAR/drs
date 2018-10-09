91f6bbeaa6ad8f30eb8786b439ff1e2971006de3
Hiba Jamal <hj04033@st.habib.edu.pk>
2018-10-09 12:55:18 +0500

    add screen - sign up as a user

--- routes/signup.js
+++ routes/signup.js
@@ -6,4 +6,8 @@ router.get('/doctor', function(req, res, next) {
   res.render('doctor/signup', { type : 'doctor'});
 });
 
+router.get('/user', function(req, res, next) {
+    res.render('user/signup', { type: 'user' });
+});
+
 module.exports = router;
